772c7dca2b90c9c466f51fb411534f3bd7505d72
Saklain Mustak <saklainmustak@sakmac.local>
2022-04-18 12:59:54 +0600

    routing and linked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
 import CheckOut from "./Pages/CheckOut/CheckOut";
 import Home from "./Pages/Home/Home";
 import Footer from "./Pages/Shared/Footer/Footer";
@@ -10,7 +12,10 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
+        <Route pathe="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -13,12 +13,12 @@ const Header = () => {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#services">Services</Nav.Link>
-                            <Nav.Link href="#blogs">Blogs</Nav.Link>
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link href="home#blogs">Blogs</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="#deets">About</Nav.Link>
-                            <Nav.Link as={Link} to="#Login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
