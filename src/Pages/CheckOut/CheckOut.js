29c6f4aa72f08e5effd211f7dd817a1fbd142461
Saklain Mustak <saklainmustak@sakmac.local>
2022-04-19 13:51:09 +0600

    chechout updated

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CheckOut = () => {
     return (
         <div style={{height:"300px"}} className='w-100 d-flex justify-content-center align-items-center'>
-            <h1>Welcome In our Service 🤡</h1>
+            <h1>Welcome To our Service 🤡</h1>
             
         </div>
     );
